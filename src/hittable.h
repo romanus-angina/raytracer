05772aad1be8819402d9eae9c84b6006d5818270
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-08-22 13:42:20 -0500

    feat: Implemented hittable abstract class, sphere class, and outward face detetction

---
+++ src/hittable.h
@@ -0,0 +1,26 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class hit_record{
+    public:
+        point3 p; // Point of intersection
+        vec3 normal; // Normal at the intersection point
+        double t; // Parameter t for the ray equation
+        bool front_face; // Indicates if the ray hit the front or back face of the object
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
+};
+
+class hittable {
+    public:
+        virtual ~hittable() = default;
+
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+#endif // HITTABLE_H
---
+++ src/sphere.h
@@ -0,0 +1,44 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+    public:
+        sphere(const point3& center, double radius): center(center), radius(std::fmax(0, radius)) {}
+
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec){
+            vec3 oc = center - r.origin();
+            auto a = r.direction().length_squared();
+            auto h = dot(r.direction(), (oc));
+            auto c = oc.length_squared() - radius * radius;
+            auto discriminant = h * h - a * c;
+                if (discriminant < 0) {
+                    return false; // No intersection
+                } 
+                auto sqrtd = std::sqrt(discriminant);
+
+                //Calculate nearest root in acceptable range
+                auto root = (-h - sqrtd) / a;
+                if (root <= ray_tmin || ray_tmax <= root) {
+                    root = (h + sqrtd) / a;
+                    if (root <= ray_tmin || ray_tmax <= ray_tmax) {
+                        return false; // No valid intersection
+                    }
+                }
+                rec.t = root;
+                rec.p = r.at(rec.t);
+                vec3 outward_normal = (rec.p - center) / radius; // Normal at the intersection point
+                rec.set_face_normal(r, outward_normal); // Set the normal and front face
+                return true; // Intersection found
+            
+        }
+        
+
+    private:
+        point3 center;
+        double radius;
+};
+
+#endif // SPHERE_H
