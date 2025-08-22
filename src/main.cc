591c4afda6ec459dda5b2a7038bb41ceca4e4aa5
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-08-22 01:42:57 -0500

    feat: Implemented colors to generate simple image graphic

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
 # include <iostream>
+# include "vec3.h"
+# include "color.h"
 
 int main(){
     // Image
@@ -13,15 +15,8 @@ int main(){
     for (int j=0; j < image_height; j++){
         std::clog << "\rScanlines remaining: " << j << " of " << image_height << "\n";
         for (int i=0; i<image_width; i++){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << " " << ig << " " << ib << "\n";
+           auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
+           write_color(std::cout, pixel_color);
         }
     }
 
