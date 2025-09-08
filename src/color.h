643d3b69d2cdc0de7dd8b3170ce3142ab493cff5
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-08 10:33:18 -0500

    feat: Gamma corrected color values

--- src/color.h
+++ src/color.h
@@ -6,10 +6,21 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component){
+    if(linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0.0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color){
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+    // Apply gamma correction with gamma=2.0.   
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Write the translated [0,255] value of each color component.
     static const interval intensity(0.0, 0.999);
