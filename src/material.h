723e6bfa7c8852f1f23c37a171d8a41fe39e4b0d
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-09 23:52:59 -0500

    feat: Implemented lambertian class

--- src/material.h
+++ src/material.h
@@ -13,4 +13,21 @@ class material {
         };
 
 };
+
+class lambertian: public material {
+    public:
+        lambertian(const color& albedo): albedo(albedo) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            auto scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+
+    private:
+        color albedo;
+}
 # endif // MATERIAL_H
