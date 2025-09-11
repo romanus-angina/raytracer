45760f9e74f47f74fb324b0d6bffd4e869ed5f36
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-11 11:51:38 -0500

    feat: Implemented hollow glass sphere scene

--- src/main.cc
+++ src/main.cc
@@ -11,7 +11,8 @@ int main(){
     // World materials
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<dielectric>(1.0/1.33);
+    auto material_left = std::make_shared<dielectric>(1.50);
+    auto material_left_inner = std::make_shared<dielectric>(1.00/1.50);
     auto material_right =std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     // World
@@ -19,6 +20,7 @@ int main(){
     world.add(std::make_shared<sphere>(point3(0,0,-1.2), 0.5, material_center));
     world.add(std::make_shared<sphere>(point3(0,-100.5,-1.0), 100, material_ground));
     world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_left_inner)); // Hollow sphere for dielectric
     world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
