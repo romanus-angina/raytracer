82b7ea9ccb6978a705e185ec60c3e66016da66e9
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-11 11:46:53 -0500

    feat: implemented full glass material

--- src/material.h
+++ src/material.h
@@ -65,9 +65,9 @@ class dielectric: public material {
             double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
             double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
             bool cannot_refract = ri * sin_theta > 1.0;
-            // Schlick's approximation for reflectance
+            
             vec3 direction;
-            if (cannot_refract)
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double())
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -77,5 +77,12 @@ class dielectric: public material {
         }
     private:
         double refraction_index; // Index of Refraction
+
+        // Schlick's approximation for reflectance
+        static double reflectance(double cosine, double refraction_index){
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+        }
 };
 # endif // MATERIAL_H
