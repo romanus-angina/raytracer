ab7c5f0633b0b1175faf172d806bea4731fcdcac
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-10 23:37:18 -0500

    feat: redid ray_color function in accordance with new attenuation property of materials

--- src/camera.h
+++ src/camera.h
@@ -1,6 +1,7 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 #include "hittable.h"
+#include "material.h"
 class camera{
     public:
     double aspect_ratio = 1.0; // Ratio of image width over image height
@@ -71,8 +72,11 @@ class camera{
         }
         hit_record rec;
         if (world.hit(r, interval(0.001, infinity), rec)){
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5*(ray_color(ray(rec.p, direction), depth -1, world));
+            ray scattered;
+            color attenuation;
+            if(rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth - 1, world);
+            return color(0,0,0);
         }
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
