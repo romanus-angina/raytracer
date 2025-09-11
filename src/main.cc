c655207e6ed8f2ed9e3fc45cf53e6f01489596f2
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-11 00:08:40 -0500

    feat: created scene with 3 speres, one lambertian and 2 metallic

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,25 @@
 # include "constants.h"
 # include "hittable.h"
 # include "hittable_list.h"
+# include "material.h"
 # include "sphere.h"
 # include "interval.h"
 # include "camera.h"
 
 
 int main(){
+    // World materials
+    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right =std::make_shared<metal>(color(0.8, 0.6, 0.2));
 
     // World
     hittable_list world;
-    world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100));
+    world.add(std::make_shared<sphere>(point3(0,0,-1.2), 0.5, material_center));
+    world.add(std::make_shared<sphere>(point3(0,-100.5,-1.0), 100, material_ground));
+    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
     camera cam;
