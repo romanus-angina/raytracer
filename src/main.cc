e42b7c016449e49b14a438067029eeed002f57f6
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-09-12 23:09:36 -0500

    chore: test render scene

--- src/main.cc
+++ src/main.cc
@@ -8,20 +8,17 @@
 
 
 int main(){
+    auto R = cos(pi/4);
+    
     // World materials
-    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<dielectric>(1.50);
-    auto material_left_inner = std::make_shared<dielectric>(1.00/1.50);
-    auto material_right =std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_left = std::make_shared<lambertian>(color(0, 0, 1));
+    auto material_right =std::make_shared<lambertian>(color(1, 0, 0));
 
     // World
     hittable_list world;
-    world.add(std::make_shared<sphere>(point3(0,0,-1.2), 0.5, material_center));
-    world.add(std::make_shared<sphere>(point3(0,-100.5,-1.0), 100, material_ground));
-    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_left_inner)); // Hollow sphere for dielectric
-    world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+    world.add(std::make_shared<sphere>(point3(-R, 0.0, -1.0), R, material_left));
+    world.add(std::make_shared<sphere>(point3(R, 0.0, -1.0), R, material_right));
 
     // Camera
     camera cam;
@@ -30,6 +27,7 @@ int main(){
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+    cam.vfov = 90.0;
 
     // Render
     cam.render(world);
