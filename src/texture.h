7ff084fd6e8c592369801cdb055798b2201a343b
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-01-20 11:46:31 -0600

    feat: shifted phase on perlin noise resulting in marble render

--- src/texture.h
+++ src/texture.h
@@ -81,7 +81,7 @@ class noise_texture: public texture {
         noise_texture(double scale) : scale(scale) {}
 
         color value(double u, double v, const point3& p) const override {
-            return color(1,1,1) * noise.turb(scale * p, 7); 
+            return color(0.5,0.5,0.5) * (1 + sin(scale * p.z() + 10 * noise.turb(p, 7))); 
         }
     private:
         perlin noise;
