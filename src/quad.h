7a67a22b12ff6cb369dc2b710ebdeabeceee3e32
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-01-22 11:05:03 -0600

    feat: implemented initial quad hit method

--- src/quad.h
+++ src/quad.h
@@ -6,6 +6,9 @@ class quad : public hittable {
     public:
         quad(const point3& Q, const vec3& u, vec3& v, shared_ptr<material> mat)
         : Q(Q), u(u), v(v), mat(mat) {
+            auto n = cross(u, v);
+            normal = unit_vector(n);
+            D = dot(normal, Q);
             set_bounding_box();
         }
 
@@ -20,7 +23,26 @@ class quad : public hittable {
         }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
-            return false; // To be implemented
+            auto denom = dot(normal, r.direction());
+            if(std::fabs(denom) < 1e-8) {
+                return false; // Ray is parallel to the quad
+            }
+
+            auto t = (D - dot(normal, r.origin())) / denom;
+
+            if(!ray_t.contains(t)) {
+                return false; // Intersection is outside the ray's valid t interval
+            }
+
+            auto intersection = r.at(t);
+
+            rec.t = t;
+            rec.p = intersection;
+            rec.mat = mat;
+
+            rec.set_face_normal(r, normal);
+
+            return true;
         }
     private:
         point3 Q; // One corner of the quad
@@ -28,6 +50,8 @@ class quad : public hittable {
         vec3 v;   // Edge vector from Q
         shared_ptr<material> mat;   // Material of the quad
         aabb bbox; // Axis-aligned bounding box
+        vec3 normal; // Normal vector of the quad
+        double D; // Plane constant
 };
 
 #endif
