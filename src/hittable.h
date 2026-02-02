bad9b33345daf69a59874f4c2c482d6ffe3bf900
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-02-01 22:18:26 -0500

    feat: instance translation

--- src/hittable.h
+++ src/hittable.h
@@ -28,4 +28,28 @@ class hittable {
         virtual aabb bounding_box() const = 0;
 };
 
+class translate: public hittable {
+    public:
+        translate(shared_ptr<hittable> object, const vec3& offset): object(object), offset(offset) {
+            bbox = object->bounding_box() + offset;
+        }
+
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+            // Move ray by offset backwards
+            ray offset_r(r.origin() - offset, r.direction(), r.time());
+            // Check for intersectio on offset ray
+            if(!object->hit(offset_r, ray_t, rec)) return false;
+            // Mobe intersection forward by offset
+            rec.p += offset;
+            return true;
+        }
+
+        aabb bounding_box() const override {return bbox; }
+    private:
+        shared_ptr<hittable> object;
+        vec3 offset;
+        aabb bbox;
+
+};
+
 #endif // HITTABLE_H
