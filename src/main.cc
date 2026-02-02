cc4b696166705403da2757eb7d96b2df47565d06
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-02-01 21:55:15 -0500

    feat: created box function to create cubes/boxes and completed cornell box render

--- src/main.cc
+++ src/main.cc
@@ -222,6 +222,9 @@ void cornell_box() {
     world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
     world.add(make_shared<quad>(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), light));
 
+    world.add(box(point3(130,0,65),point3(295,165,230), white));
+    world.add(box(point3(265,0,295),point3(430,330,460), white));
+
     // Camera
     camera cam;
     
--- src/quad.h
+++ src/quad.h
@@ -1,6 +1,7 @@
 #ifndef QUAD_H
 #define QUAD_H
 #include "hittable.h"
+#include "hittable_list.h"
 
 class quad : public hittable {
     public:
@@ -74,5 +75,27 @@ class quad : public hittable {
         vec3 normal; // Normal vector of the quad
         double D; // Plane constant
 };
+
+inline shared_ptr<hittable_list> box(const point3& a, const point3& b, shared_ptr<material> mat){
+            // Return a 3D box/cube withh opposite vertices a and b
+            auto sides = make_shared<hittable_list>();
+
+            // Construct a and b with minimum nad maximum vertices
+            auto min = point3(fmin(a.x(),b.x()), fmin(a.y(),b.y()), fmin(a.z(),b.z()));
+            auto max = point3(fmax(a.x(),b.x()), fmax(a.y(),b.y()), fmax(a.z(),b.z()));
+
+            auto dx = vec3(max.x() - min.x(), 0, 0);
+            auto dy = vec3(0, max.y() - min.y(), 0);
+            auto dz = vec3(0, 0, max.z() - min.z());
+
+            sides->add(make_shared<quad>(point3(min.x(),min.y(),max.z()),dx,dy,mat)); //front
+            sides->add(make_shared<quad>(point3(max.x(),min.y(),max.z()),-dz,dy,mat)); //right
+            sides->add(make_shared<quad>(point3(max.x(),min.y(),min.z()),-dx,dy,mat)); //back
+            sides->add(make_shared<quad>(point3(min.x(),min.y(),min.z()),dz,dy,mat)); //left
+            sides->add(make_shared<quad>(point3(min.x(),max.y(),max.z()),dx,-dz,mat)); //top
+            sides->add(make_shared<quad>(point3(min.x(),min.y(),min.z()),dx,dz,mat)); //bottom
+
+            return sides;
+        }
 
 #endif
