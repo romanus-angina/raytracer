46df55bd5e04917fd7f8e3057cfddb5a23cc7255
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-02-01 21:23:00 -0500

    render: Cornell box

--- src/main.cc
+++ src/main.cc
@@ -207,8 +207,40 @@ void simple_light(){
     // Render
     cam.render(world);
 }
+void cornell_box() {
+    hittable_list world;
+
+    auto red = make_shared<lambertian>(color(0.65,0.05,0.05));
+    auto white = make_shared<lambertian>(color(0.73,0.73,0.73));
+    auto green = make_shared<lambertian>(color(0.12,0.45,0.15));
+    auto light = make_shared<diffuse_light>(color(15,15,15));
+
+    world.add(make_shared<quad>(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red));
+    world.add(make_shared<quad>(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green));
+    world.add(make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
+    world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
+    world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
+    world.add(make_shared<quad>(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), light));
+
+    // Camera
+    camera cam;
+    
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 600;
+    cam.samples_per_pixel = 200;
+    cam.max_depth = 50;
+    cam.vfov = 40.0;
+    cam.lookfrom = point3(278,278,-800);
+    cam.lookat = point3(278,278,0);
+    cam.vup = vec3(0,1,0);
+    cam.defocus_angle = 0.0;
+    cam.background = color(0.0, 0.0, 0.0);
+
+    // Render
+    cam.render(world);
+}
 int main(){
-    switch(6){
+    switch(7){
         case 1:
             bouncing_spheres();
             break;
@@ -226,6 +258,9 @@ int main(){
             break;
         case 6:
             simple_light();
+            break;
+        case 7:
+            cornell_box();
             break;
         default:
             std::cerr << "Invalid scene selection." << std::endl;
