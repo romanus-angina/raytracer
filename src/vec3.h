139e8cd03c8cbb8ddf6429bb8dedc7d60c25af56
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-02-12 23:19:55 -0600

    Added vec3 functionalit for monte carlo implementation

--- src/vec3.h
+++ src/vec3.h
@@ -139,4 +139,12 @@ inline vec3 random_on_hemisphere(const vec3& normal){
 
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 #endif // VEC3_H
